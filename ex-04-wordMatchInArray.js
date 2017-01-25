2f1c5bc4bea128169a24ce8cb2e74bac74c54afc
Andrew Mason <andrewmason1@gmail.com>
2017-01-25 14:07:28 -0500

    assignment10-draft

---
+++ ex-04-wordMatchInArray.js
@@ -0,0 +1,39 @@
+
+/**
+ * Ex-04 : wordMatchInArray()
+ *
+ * Write a function called `wordMatchInArray` that takes 2 inputs: a string, and an array.
+ * The function should return a boolean value if that word is found in the array.
+ *
+*/
+
+var wordMatchInArray = function(string, array){
+  var result = false;
+
+  for(i=0; i<array.length; i=i+1){
+
+    if(string===array[i]){
+      result = true;
+    }
+  }
+
+  return result;
+}
+
+
+
+
+
+
+
+
+//*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*~*~*~*~
+var sampleWordsArray = ["because", "girls", "just", "want", "to", "have", "fun"]
+
+console.assert(wordMatchInArray('girls', sampleWordsArray) === true)
+console.assert(wordMatchInArray('fun', sampleWordsArray) === true)
+console.assert(wordMatchInArray('boys', sampleWordsArray) === false)
+
+var sampleWordsArray2 = ["boys", "there", "is", "no", "way", "out", "of", "here"]
+console.assert(wordMatchInArray('boys', sampleWordsArray2) === true)
+console.assert(wordMatchInArray('girls', sampleWordsArray2) === false)
---
+++ ex-06-reverseString.js
@@ -0,0 +1,28 @@
+
+/**
+ * Ex-09 : reverseString()
+ *
+ * Write a function called reverseString that takes a string as input
+ * and returns a string with the characters in reverse order.
+ * (note: can't use .reverse() method )
+ *
+*/
+
+var reverseString = function(string){
+  var newString = '';
+
+  for(i=string.length-1; i>=0; i=i-1){
+    newString = newString+string[i];
+  }
+  return newString;
+}
+
+
+
+
+
+//*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*~*~*~*~
+console.assert( reverseString('books') === 'skoob')
+console.assert( reverseString('coolness') === 'ssenlooc')
+console.assert( reverseString('bedtime') === 'emitdeb')
+console.assert( reverseString('yah sure') === 'erus hay')
